5c511c671f924021677400ed895c68faef56b774
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2022-10-22 18:05:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 # CPU 占用限制
 PACKAGES="$PACKAGES luci-i18n-cpulimit-zh-cn"
 # collectd 统计
-PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 
 # 常用软件服务
 
@@ -27,7 +27,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 # Passwall 代理
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 # frp 和 nps 内网穿透
-PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
 
 # 常用的网络存储组件
 
@@ -36,15 +36,15 @@ PACKAGES="$PACKAGES luci-app-fileassistant"
 # 硬盘休眠
 PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
 # p910nd 打印服务器
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
 # GoWebDav
-PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
 # Samba 网络共享
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # qBittorrent 下载器
 PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
 # Aria2 下载器
-PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
 # FTP 服务器
 PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 
@@ -66,28 +66,39 @@ PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
 PACKAGES="$PACKAGES luci-app-syncdial luci-i18n-mwan3-zh-cn"
 # 宽带监控 Nlbwmon
 PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
+# ip/mac 绑定
+PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
+# 流量智能队列管理(QOS)
+PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
+# Turbo ACC 网络加速
+PACKAGES="$PACKAGES luci-i18n-turboacc-zh-cn kmod-shortcut-fe-cm"
 
 # 界面翻译补全
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-turboacc-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
 
 # Packages 文件夹下的 ipk 包
 PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
 
 # 一些自己需要的内核组件
-PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
+# PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
 
 # 一些其他可能有用的包
 
 # zsh 终端
-PACKAGES="$PACKAGES zsh"
+# PACKAGES="$PACKAGES zsh"
 # Vim 完整版，带语法高亮
-PACKAGES="$PACKAGES vim-fuller"
+PACKAGES="$PACKAGES vim-fuller nano-plus"
 # X/Y/ZMODEM 文件传输
 PACKAGES="$PACKAGES lrzsz"
 # OpenSSH
 PACKAGES="$PACKAGES openssh-server openssh-client"
-# Netdata 系统监控界面
-PACKAGES="$PACKAGES netdata"
+# Wireguard
+PACKAGES="$PACKAGES luci-i18n-wireguard-zh-cn"
+# ipv6
+PACKAGES="$PACKAGES ipv6helper ip6tables-extra ip6tables-mod-nat"
+
+PACKAGES="$PACKAGES iperf3 htop"
 
 # 一些自定义文件
 FILES="files"
