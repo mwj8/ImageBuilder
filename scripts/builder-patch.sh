bd9c61c25ef1d2e02b8292ff4414a9bfcd02da8a
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2022-10-22 17:47:39 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -6,7 +6,7 @@ echo "Current Path: $PWD"
 cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 
 # Remove redundant default packages
-sed -i "/luci-app-cpufreq/d" include/target.mk
+#sed -i "/luci-app-cpufreq/d" include/target.mk
 
 # Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
@@ -16,3 +16,11 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
+
+sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
+sed -i "s/CONFIG_VDI_IMAGES=y/# CONFIG_VDI_IMAGES is not set/" .config
+sed -i "s/CONFIG_VMDK_IMAGES=y/# CONFIG_VMDK_IMAGES is not set/" .config
+
+sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=16/CONFIG_TARGET_KERNEL_PARTSIZE=32/" .config
+
+sed -i 's/21.02-SNAPSHOT/21.02.2/g' repositories.conf
