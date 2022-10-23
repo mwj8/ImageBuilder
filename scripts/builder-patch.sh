bee2254c78ae29e2846ee7e298f44d0738e5b00a
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2022-10-23 17:15:55 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -23,4 +23,4 @@ sed -i "s/CONFIG_VMDK_IMAGES=y/# CONFIG_VMDK_IMAGES is not set/" .config
 
 sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=16/CONFIG_TARGET_KERNEL_PARTSIZE=32/" .config
 
-sed -i 's/21.02-SNAPSHOT/21.02.2/g' repositories.conf
+#sed -i 's/21.02-SNAPSHOT/21.02.2/g' repositories.conf
