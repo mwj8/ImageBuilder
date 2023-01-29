803228f2d59c4ed21226f3d3bcfff427624cf7c6
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2023-01-30 01:45:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ PACKAGES="$PACKAGES luci-i18n-ramfree-zh-cn"
 # 常用主题
 
 # Argon 主题
-# PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-theme-material"
+PACKAGES="$PACKAGES luci-theme-argon"
+PACKAGES="$PACKAGES luci-theme-openwrt-2020"
 
 # 常用系统管理组件
 
@@ -29,12 +29,10 @@ PACKAGES="$PACKAGES luci-i18n-webadmin-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 # CPU 占用限制
 # PACKAGES="$PACKAGES luci-i18n-cpulimit-zh-cn"
-# collectd 统计
-# PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 # ttyd终端
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # 定时重启
-PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
 
 # 常用软件服务
 
@@ -43,9 +41,10 @@ PACKAGES="$PACKAGES luci-i18n-ssr-plus-zh-cn"
 # OpenClash 代理
 PACKAGES="$PACKAGES luci-app-openclash"
 # Passwall 代理
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn chinadns-ng hysteria naiveproxy xray-plugin"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn hysteria naiveproxy ipt2socks"
 # frp 和 nps 内网穿透
-PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-nps-zh-cn"
 # upnp端口自动转发
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 # 阿里云盘webdav
@@ -86,7 +85,7 @@ PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
 # NFS 管理
 PACKAGES="$PACKAGES luci-i18n-nfs-zh-cn"
 # alist
-PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
 
 # Docker 组件
 # PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn docker-compose"
@@ -106,22 +105,23 @@ PACKAGES="$PACKAGES luci-i18n-wireguard-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
 # 多线多拨与负载均衡
 # PACKAGES="$PACKAGES luci-app-syncdial luci-i18n-mwan3-zh-cn luci-i18n-mwan3helper-zh-cn"
-# 宽带监控 Nlbwmon
-PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 # ip/mac 绑定
 PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
 # 流量智能队列管理(QOS)
 PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
 # Turbo ACC 网络加速
-PACKAGES="$PACKAGES luci-i18n-turboacc-zh-cn kmod-tcp-bbr "
+PACKAGES="$PACKAGES luci-i18n-turboacc-zh-cn kmod-tcp-bbr"
 # socat
 PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
 
-# 界面翻译补全
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-filetransfer-zh-cn"
+# 宽带监控 Nlbwmon
+# PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 
 # Packages 文件夹下的 ipk 包
-PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
+
+# 界面翻译补全
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-filetransfer-zh-cn"
 
 # 一些自己需要的内核组件
 # PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
@@ -138,12 +138,12 @@ PACKAGES="$PACKAGES vim-fuller nano-plus"
 # OpenSSH
 PACKAGES="$PACKAGES openssh-sftp-client openssh-sftp-server"
 # ipv6
-PACKAGES="$PACKAGES ipv6helper ip6tables-extra ip6tables-mod-nat"
+PACKAGES="$PACKAGES ipv6helper ip6tables-extra"
 
 PACKAGES="$PACKAGES iperf3 htop"
 
 #去除
-PACKAGES="$PACKAGES -samba36-server"
+# PACKAGES="$PACKAGES -samba36-server"
 
 # 一些自定义文件
 FILES="files"
