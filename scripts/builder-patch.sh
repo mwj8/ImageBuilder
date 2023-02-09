fc390dcad16ae920b89b65ec3ab6b76c7ca2ba83
SharpCode <46841497+SharpCode-cn@users.noreply.github.com>
2023-02-09 09:56:50 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -9,7 +9,7 @@ cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 #sed -i "/luci-app-cpufreq/d" include/target.mk
 
 # Force opkg to overwrite files
-# sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
+sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
 
 # Not generate ISO images for it is too big
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
